802ac795cbbdaaadf9fa8064667c7b1b7996da4a
THEBLESSMAN687 <JUAN.RAVE@UTP.EDU.CO>
2025-10-05 11:55:46 -0500

    Add encoding declaration to Python script
    Added '# coding=utf-8' to the top of demo_questionnaire_driven_system.py to explicitly declare file encoding. This helps ensure proper handling of non-ASCII characters.

--- demo_questionnaire_driven_system.py
+++ demo_questionnaire_driven_system.py
@@ -1,4 +1,5 @@
-?#!/usr/bin/env python3
+# coding=utf-8
+#!/usr/bin/env python3
 """
 DEMOSTRACIÓN EJECUTABLE: Sistema Orientado por Cuestionario
 ============================================================
